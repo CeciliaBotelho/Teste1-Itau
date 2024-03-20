0581118b1407b3f855a7d3c276aaac9c29ddcc55
CeciliaBotelho <ceciliacbcbcb@gmail.com>
2024-03-20 00:04:40 -0300

    adicionando

--- api/src/main/java/br/com/projeto/api/repositorio/Repositorio.java
+++ api/src/main/java/br/com/projeto/api/repositorio/Repositorio.java
@@ -4,6 +4,8 @@ import org.springframework.data.repository.CrudRepository;
 
 import br.com.projeto.api.modelo.Task;
 
+//Task é o tipo de entidade que p repositorio oferece operações
+//long é o tipo da chave primaria
 public interface Repositorio  extends CrudRepository<Task, Long>{
 
 
